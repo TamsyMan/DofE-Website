05f82b368285b687fe78a38244d27375e8087204
RafeElliff <rafe.elliff@westminster.org.uk>
2023-10-08 16:49:17 +0100

    Research more HTML and create input and output
    Research HTML
    Edit index.html and main code
    Fix bugs
    Create output website where output is shown

--- FlaskWebsite/main.py
+++ FlaskWebsite/main.py
@@ -1,30 +1,33 @@
 from flask import Flask, request, render_template
+from werkzeug.exceptions import BadRequestKeyError
 
 app = Flask(__name__)
 
 @app.route('/')
-def siteOpened():
-    return render_template('index.html')
-
-
-@app.route('/', methods = ["GET", "POST"])
+def initialise():
+    return(render_template('index.html'))
+@app.route('/calculator', methods = ["GET", "POST"])
 def calculate():
-    if request.method == "POST":
-        number1 = request.form('number1')
-        calc = request.form('calc')
-        number2 = request.form('number2')
-        print (number1)
-        if calc == "+":
-            print (number1 + number2)
-        elif calc == "-":
-            print (number1 - number2)
-        elif calc == "*":
-            print (number1*number2)
-        elif calc == "/":
-            print (number1/number2)
-        else:
-            print ("Not a valid sum")
-        return render_template('index.html')
+    try:
+        number1 = int(request.form["number1"])
+        calc = request.form["calc"]
+        number2 = int(request.form["number2"])
+    except BadRequestKeyError as error:
+        return "Missing Form Field"
+
+    if calc == "+":
+        result = (number1 + number2)
+    elif calc == "-":
+        result = (number1 - number2)
+    elif calc == "*":
+        result = (number1*number2)
+    elif calc == "/":
+        result = (number1/number2)
+    else:
+        print ("Not a valid sum")
+    print (result)
+    return render_template('index.html', result=result)
 
 
-app.run()
+
+app.run(debug=True)
