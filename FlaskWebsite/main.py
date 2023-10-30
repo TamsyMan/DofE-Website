da36b3c6e9339a860fb7e988c9ca9e88088a4473
RafeElliff <rafe.elliff@westminster.org.uk>
2023-10-30 20:47:50 +0000

    Work more on outputs Adjust index.html Create new html file for calculator output.
    (Not full hour - will pick up again at some point this week)

--- FlaskWebsite/main.py
+++ FlaskWebsite/main.py
@@ -6,27 +6,25 @@ app = Flask(__name__)
 @app.route('/')
 def initialise():
     return(render_template('index.html'))
-@app.route('/calculator', methods = ["GET", "POST"])
+@app.route('/calculator', methods = ["GET"])
 def calculate():
     try:
         number1 = int(request.form["number1"])
         calc = request.form["calc"]
         number2 = int(request.form["number2"])
-    except BadRequestKeyError as error:
+        if calc == "+":
+            result = (number1 + number2)
+        elif calc == "-":
+            result = (number1 - number2)
+        elif calc == "*":
+            result = (number1 * number2)
+        elif calc == "/":
+            result = (number1 / number2)
+        else:
+            result = "Not a valid sum"
+        return render_template('calculator.html', result=result)
+    except KeyError as error:
         return "Missing Form Field"
-
-    if calc == "+":
-        result = (number1 + number2)
-    elif calc == "-":
-        result = (number1 - number2)
-    elif calc == "*":
-        result = (number1*number2)
-    elif calc == "/":
-        result = (number1/number2)
-    else:
-        print ("Not a valid sum")
-    print (result)
-    return render_template('index.html', result=result)
 
 
 
