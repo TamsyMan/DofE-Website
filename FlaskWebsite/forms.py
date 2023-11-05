ff9b53f8677e3785bd5215b45b2134937141e69d
RafeElliff <rafe.elliff@westminster.org.uk>
2023-11-05 12:35:46 +0000

    Change forms to use WTForms framework

---
+++ FlaskWebsite/forms.py
@@ -0,0 +1,7 @@
+from wtforms import Form, IntegerField, StringField, SubmitField
+
+class CalcForm(Form):
+    Number1=IntegerField('Number 1')
+    Calculation=StringField('Calculation')
+    Number2=IntegerField('Number 2')
+    Submit=SubmitField('Submit')
--- FlaskWebsite/main.py
+++ FlaskWebsite/main.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, render_template
+from forms import CalcForm
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def initialise():
-    return(render_template('index.html'))
+    form = CalcForm(request.form)
+    return(render_template('index.html', form=form))
 @app.route('/calculator', methods = ["POST"])
 def calculate():
     try:
-        number1 = int(request.form["number1"])
-        calc = request.form["calc"]
-        number2 = int(request.form["number2"])
+        number1 = int(request.form["Number1"])
+        calc = request.form["Calculation"]
+        number2 = int(request.form["Number2"])
         if calc == "+":
             result = (number1 + number2)
         elif calc == "-":
