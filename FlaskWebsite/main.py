0fbe5bbd544579d7e6fd3a4385f2fbddcd12a15b
RafeElliff <rafe.elliff@westminster.org.uk>
2023-11-15 20:02:55 +0000

    Adjust calculator to display result on same page as input

--- FlaskWebsite/main.py
+++ FlaskWebsite/main.py
@@ -9,29 +9,27 @@ def initialise():
     return render_template('index.html')
 
 
-@app.route('/calculator')
+@app.route('/calculator', methods = ["GET", "POST"])
 def calculator():
     form = CalcForm(request.form)
-    return render_template('calculator.html', form=form)
-
-@app.route('/calcresult', methods = ["POST"])
-def calcResult():
-    try:
-        number1 = int(request.form["Number1"])
-        calc = request.form["Calculation"]
-        number2 = int(request.form["Number2"])
-        if calc == "+":
-            result = (number1 + number2)
-        elif calc == "-":
-            result = (number1 - number2)
-        elif calc == "*":
-            result = (number1 * number2)
-        elif calc == "/":
-            result = (number1 / number2)
-        else:
-            result = "Not a valid sum"
-        return render_template('calcresult.html', result=result)
-    except KeyError as error:
-        return "Missing Form Field"
+    result = None
+    if request.method == "POST":
+        try:
+            number1 = int(request.form["Number1"])
+            calc = request.form["Calculation"]
+            number2 = int(request.form["Number2"])
+            if calc == "+":
+                result = (number1 + number2)
+            elif calc == "-":
+                result = (number1 - number2)
+            elif calc == "*":
+                result = (number1 * number2)
+            elif calc == "/":
+                result = (number1 / number2)
+            else:
+                result = "Not a valid sum"
+        except KeyError as error:
+            return "Missing Form Field"
+    return render_template('calculator.html', form=form, result=result)
 
 app.run(debug=True)
