5aaa1d74ec99b106452795871073ff2bca01f3d8
RafeElliff <rafe.elliff@westminster.org.uk>
2023-12-10 17:48:05 +0000

    Create clock to state time, day (including correct suffix e.g. "1st", "3rd", "10th"), month (as word, not number) and year. Also says what day of week it is.

--- FlaskWebsite/main.py
+++ FlaskWebsite/main.py
@@ -6,23 +6,66 @@ import requests
 app = Flask(__name__)
 
 
+def find_time():
+    api_url = 'http://worldtimeapi.org/api/ip'
+    response = requests.get(api_url)
+    data = response.json()
+
+    datetime = data.get("datetime")
+    date_and_time = datetime.split("T")
+    date = date_and_time[0]
+    time = date_and_time[1]
+    time = time.split(".")
+    time = time[0]
+    parts_of_date = date.split("-")
+    parts_of_time = time.split(":")
+    hour = parts_of_time[0]
+    minute = parts_of_time[1]
+    second = parts_of_time[2]
+    year = parts_of_date[0]
+    month = parts_of_date[1]
+    day = parts_of_date[2]
+
+    days_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    weekday = days_of_week[data.get("day_of_week")]
+
+    all_info = {"date": date, "time": time, "hour": hour, "min": minute, "second": second,"year": year, "month": month, "day": day, "weekday": weekday}
+
+    return all_info
 
 @app.route('/')
 def initialise():
     return render_template('index.html')
 
+def date_suffix_calculator():
+    day = find_time()["day"]
+    if day[-1] == "1" and day != "11":
+        suffix = "st"
+    elif day[-1] == "2" and day != "12":
+        suffix = "nd"
+    elif day[-1] == "3" and day != "13":
+        suffix = "rd"
+    else:
+        suffix = "th"
 
+    return suffix
+
+def month_calculator():
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October', 'November', 'December']
+    month = find_time()["month"]
+    month = months[int(month)-1]
+    return month
 @app.route('/clock', methods = ["GET", "POST"])
 def clock():
-    api_url = 'http://worldtimeapi.org/api/ip'
-    days_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    response = requests.get(api_url)
-    data = response.json()
+    time = find_time()["time"]
+    day = find_time()["day"]
+    month = month_calculator()
+    year = find_time()["year"]
+    suffix = date_suffix_calculator()
+    weekday = find_time()["weekday"]
+    final_string = f"It is {time} on the {day}{suffix} of {month}, {year}. It is a {weekday}."
 
-    day_of_week = days_of_week[data.get('day_of_week')]
-    #Idea for next time: Create calculator that sees how many days until a given date?
-
-    return (render_template('clock.html', day_of_week = day_of_week))
+    return render_template('clock.html', final_string = final_string)
 
 @app.route('/calculator',  methods = ['GET', 'POST'])
 def calculator():
